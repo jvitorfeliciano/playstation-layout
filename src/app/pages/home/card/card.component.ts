ac2ccaf8020305aaea626f5fc83275d1aa43e2df
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-09-20 21:54:11 -0300

    feat: add card price

--- src/app/pages/home/card/card.component.ts
+++ src/app/pages/home/card/card.component.ts
@@ -3,9 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
   @Input()
-  gameLabel: string = "";
+  gameLabel: string = '';
+
+  @Input()
+  gameType: string = '';
+
+  @Input()
+  price: string = '';
 }
